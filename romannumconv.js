d502badb62e002f437e7ebbb9f1ebd251bf8459f
UtpalB-arch <67811773+UtpalB-arch@users.noreply.github.com>
2021-01-25 06:11:20 +1100

    Update romannumconv.js

--- romannumconv.js
+++ romannumconv.js
@@ -6,24 +6,7 @@ function convertToRoman(num)
     let hundRem=0; let hundQuo=0;
     let romThou=''; let romHun=''; let romTen=''; let romUnit='';
 
-    let romanArray = ['I','II','III','IV','V','VI','VII','VIII','IX','X','XX','XXX','XL','L','LX','LXX','LXXX','XC','C','CC','CCC','CD','D','DC','DCC','DCCC','CM','M'];
-    let decimalArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000];
-   
-    let numString = num.toString(); let numlength = numString.length;  
-    //console.log("The numString is: "+numString);console.log("The numlength is: "+numlength);
-
-    //### The breaking down of the number into thousands, hundreds, tens and units ###
-    if(numlength > 3){thouQuo = Math.floor(num/1000); thouRem = num%1000;
-        hundQuo = Math.floor(thouRem/100); hundRem = thouRem%100;
-        tensQuo = Math.floor(hundRem/10); tensRem = hundRem%10;
-        unitsQuo = tensRem;}
-    else if(numlength > 2){hundQuo = Math.floor(num/100); hundRem = num%100;
-        tensQuo = Math.floor(hundRem/10); tensRem = hundRem%10;
-        unitsQuo = tensRem;}
-    else if(numlength > 1){tensQuo = Math.floor(num/10); tensRem = num%10;
-        unitsQuo = tensRem;}
-    else if(numlength > 0){unitsQuo = num;}
-    //console.log("The thousandQ is: "+thouQuo, " The hundredQ is: "+hundQuo, " The tenQ is: "+tensQuo, " The unitQ is: "+unitsQuo);  
+   XXXXXXXXXXXXXXXXXXX PORTION OF CODE DELETED TO PROTECT INTELLECTUAL PROPERTY XXXXXXXXXXXXXXXXXXXXXXXXXXX
     
     //### The conversion into Roman numerals
     if(thouQuo > 0 && thouQuo < 4){for(let i = 0; i < thouQuo; i++){romThou += romanArray[decimalArray.indexOf(1000)];}}
